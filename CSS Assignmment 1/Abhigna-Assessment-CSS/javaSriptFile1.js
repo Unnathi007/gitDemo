4f683b62e577e2f10601d6928855bbeb29f9549d
abhigna22001 <113578288+abhigna22001@users.noreply.github.com>
2022-10-17 15:28:13 +0530

    Add files via upload

---
+++ CSS Assignmment 1/Abhigna-Assessment-CSS/javaSriptFile1.js
@@ -0,0 +1,6 @@
+function fun(name,num1,num2){
+    console.log("hello world! ",name);
+    console.log(`hello ${name}`);
+    console.log(`sum of ${num1} , ${num2} = ${num1+num2}`)
+}
+fun("abhigna",5,6);
