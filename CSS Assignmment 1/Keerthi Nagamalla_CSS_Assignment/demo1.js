68a0aecbed7454923487ecbc992f64d20eee1f5b
Keerthi9381 <83539907+Keerthi9381@users.noreply.github.com>
2022-10-17 14:26:22 +0530

    Add files via upload

---
+++ CSS Assignmment 1/Keerthi Nagamalla_CSS_Assignment/demo1.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+function sayhello(name){
+console.log("hello");
+console.log(`hello ${name}`);
+
+}
+sayhello("kite");
+
+function sum(a,b){
+    console.log(`The sum of 2 numbers ${a} and ${b} is ${a+b}`);
+}
+sum(5,5);
