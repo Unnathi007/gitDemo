23b0b9b3887c1c80e477a4d97555fc37d9194af2
Venkat Sai Kota <venkatasai357357@gmail.com>
2022-10-17 13:43:20 +0530

    CSS folder

---
+++ HTML Assignment 1/Maroju_Srikruthi_Assessment1/script.js
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <script>
+    function test(){
+	var uid = document.getElementById("username").value;
+	var pw = document.getElementById("password").value;
+	var email = document.getElementById("email").value;
+	
+	
+}
+  </script>
+</head>
+
+<body onload="test()">
+
+  <h1>"Username: " <span id="username"></span></h1>
+  <h1>"Password: " <span id="password"></span></h1>
+  <h1>"Email: " <span id="email"></span></h1>
+
+</body>
+
+</html>
