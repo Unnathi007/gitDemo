1fdcd63ac66fefd89321833975a7ad1a4235b5e2
ThugNinja4708 <58839218+ThugNinja4708@users.noreply.github.com>
2022-10-25 22:47:20 +0530

    Add files via upload

---
+++ JS Assignement 1/Rithvik Kantha Js Demos/Rithvik Kantha Js Demos/day8/import.js
@@ -0,0 +1,2 @@
+import all from "./export1.js"
+all();
---
+++ JS Assignement 1/Rithvik Kantha Js Demos/Rithvik Kantha Js Demos/day8/useStrict.js
@@ -0,0 +1,18 @@
+y = 2
+  "use strict"
+function myFunc(){
+    x = 10
+
+    console.log(x)
+}
+myFunc()
+
+ //console.log(x);
+
+//cookies 
+//variables and theming 
+//jason
+//DOM nav and man
+//debugging 
+
+//vad rad anta
